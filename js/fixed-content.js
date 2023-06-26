1217b79cea478bb2c217e10dd4ecfaa3c949936a
praktikant <praktikant@baseworks.de>
2023-06-26 16:36:14 +0200

    gg

--- js/fixed-content.js
+++ js/fixed-content.js
@@ -2,48 +2,48 @@ $(function () {
     $(".overview-about").hover(function () {
         $(".overview-item-about").animate({
             width: "40px"
-        }, 200)
+        }, 100)
         $(".overview-link-about").animate({
             marginLeft: "50px"
-        }, 200)
+        }, 100)
     }, function () {
         $(".overview-item-about").animate({
             width: "10px"
-        }, 200)
+        }, 100)
         $(".overview-link-about").animate({
             marginLeft: "12px"
-        }, 200)
+        }, 100)
     })
 
     $(".overview-experience").hover(function () {
         $(".overview-item-experience").animate({
             width: "40px"
-        }, 200)
+        }, 100)
         $(".overview-link-experience").animate({
             marginLeft: "50px"
-        }, 200)
+        }, 100)
     }, function () {
         $(".overview-item-experience").animate({
             width: "10px"
-        }, 200)
+        }, 100)
         $(".overview-link-experience").animate({
             marginLeft: "12px"
-        }, 200)
+        }, 100)
     })
 
     $(".overview-projects").hover(function () {
         $(".overview-item-projects").animate({
             width: "40px"
-        }, 200)
+        }, 100)
         $(".overview-link-projects").animate({
             marginLeft: "50px"
-        }, 200)
+        }, 100)
     }, function () {
         $(".overview-item-projects").animate({
             width: "10px"
-        }, 200)
+        }, 100)
         $(".overview-link-projects").animate({
             marginLeft: "12px"
-        }, 200)
+        }, 100)
     })
 })
---
+++ js/moving-content.js
@@ -0,0 +1,19 @@
+const TodoAppLink = document.querySelector('.project-git-link-todoapp');
+const WebDirectory = document.querySelector('.project-git-link-webdirectory');
+const Elbigote = document.querySelector('.project-git-link-elbigote');
+
+TodoAppLink.addEventListener('hover', () => {
+   alert("jkl")
+});
+
+TodoAppLink.addEventListener('click', () => {
+   window.location.href = "https://brittanychiang.com/"
+});
+
+WebDirectory.addEventListener('click', () => {
+   window.location.href = "https://brittanychiang.com/"
+});
+
+Elbigote.addEventListener('click', () => {
+   window.location.href = "https://brittanychiang.com/"
+});
