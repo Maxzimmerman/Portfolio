8108e6679099e20035bb9f2ccf941366dcc82a5d
praktikant <praktikant@baseworks.de>
2023-06-26 16:55:04 +0200

    gg

--- js/moving-content.js
+++ js/moving-content.js
@@ -2,18 +2,17 @@ const TodoAppLink = document.querySelector('.project-git-link-todoapp');
 const WebDirectory = document.querySelector('.project-git-link-webdirectory');
 const Elbigote = document.querySelector('.project-git-link-elbigote');
 
-TodoAppLink.addEventListener('hover', () => {
-   alert("jkl")
-});
-
 TodoAppLink.addEventListener('click', () => {
-   window.location.href = "https://brittanychiang.com/"
+   window.location='https://www.example.com';
+   console.log("worked")
 });
 
 WebDirectory.addEventListener('click', () => {
    window.location.href = "https://brittanychiang.com/"
+   console.log("worked")
 });
 
 Elbigote.addEventListener('click', () => {
    window.location.href = "https://brittanychiang.com/"
+   console.log("worked")
 });
